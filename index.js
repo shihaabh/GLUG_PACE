d4584b11561186f3a25bcee8ca93562742417131
Shihaab Hassan <108015887+shihaabh@users.noreply.github.com>
2023-06-23 17:33:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function print(){
+    const emali = document.getElementById("email");
+    const name = document.getElementById("name");
+    const password = document.getElementById("password");
+
+    // console.log(email);
+
+    // console.log(name);
+    // console.log(password);
+    const newpage = document.getElementsByClassName("display")
+    newpage.innerHTML  = "ashraf";
+}
+
